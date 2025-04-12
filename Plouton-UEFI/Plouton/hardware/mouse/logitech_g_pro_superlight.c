e1cc351b05a10eb50d7c7f0f9658186908a6c784
rain <admln@protonmail.com>
2025-04-12 22:32:33 +0200

    Added proper linking to Intel documentation and fixed bug regarding the spec

--- Plouton-UEFI/Plouton/hardware/mouse/logitech_g_pro_superlight.c
+++ Plouton-UEFI/Plouton/hardware/mouse/logitech_g_pro_superlight.c
@@ -22,10 +22,11 @@ mouseProfile_t EFIAPI initLogitechGProSuperlightMouseXHCI(MouseDriverTdCheck Mou
 	// This implementation is based on the following documents:
 	// - Intel 600 series datasheet see: https://www.intel.com/content/www/us/en/content-details/742460/intel-600-series-chipset-family-for-iot-edge-platform-controller-hub-pch-datasheet-volume-2-of-2.html
 	// - The SMM Rootkit Revisited: Fun with USB https://papers.put.as/papers/firmware/2014/schiffman2014.pdf
+	// - eXtensible Host Controller Interface for Universal Serial Bus (xHCI) https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/extensible-host-controler-interface-usb-xhci.pdf
 
 	mouseProfile_t ret = { 0, 0, 0, 0, 0, 0 };
 
-	// (Intel 18.1.11, Page 822) First we get the MBAR of the XHCI device
+	// (Intel 600 series datasheet, 18.1.11, Page 822) First we get the MBAR of the XHCI device
 	// Check if we got a valid MBAR
 	if (MBAR == 0)
 	{
@@ -37,7 +38,7 @@ mouseProfile_t EFIAPI initLogitechGProSuperlightMouseXHCI(MouseDriverTdCheck Mou
 
 	LOG_INFO("[XHC] Found MBAR %p\r\n", MBAR);
 
-	// (Intel x.x.x, Page xxx) Now that we have the MBAR we can access the Memory Mapped registers, use it to get the Device Context Array Base Pointer which contains pointers to the devices.
+	// (Intel eXtensible Host Controller Interface for USB, 5.4, Page 391) Now that we have the MBAR we can access the Memory Mapped registers, use it to get the Device Context Array Base Pointer which contains pointers to the devices.
 	EFI_PHYSICAL_ADDRESS DCAB = getDeviceContextArrayBase(MBAR);
 
 	// Check if we got a valid DCAB
@@ -51,6 +52,14 @@ mouseProfile_t EFIAPI initLogitechGProSuperlightMouseXHCI(MouseDriverTdCheck Mou
 
 	LOG_INFO("[XHC] Found DCAB %p\r\n", DCAB);
 
+	// Now as we have the DCAB, we scan all of the devices for the following specifications:
+	//  - Endpoint Context Field	- offset 0x04, size 32-bit
+	//		-	Endpoint Type		- Bits 5:3		(Intel eXtensible Host Controller Interface for USB, Table 6-9, Page 452, Endpoint Type (EP Type))
+	//		-	Max Packet Size		- Bits 31:16	(Intel eXtensible Host Controller Interface for USB, Table 6-9, Page 452, Max Packet Size)
+	// 
+	//	- Endpoint Context Field	- offset 0x10, size 32-bit
+	//		- Average TRB Length	- Bits 15:0		(Intel eXtensible Host Controller Interface for USB, Table 6-11, Page 453, Average TRB Length)
+
 	//Check for  Logitech G Pro Mouse (Superlight)
 	EFI_PHYSICAL_ADDRESS endpointRing = getEndpointRing(DCAB, 7, 0x40, 0x20, FALSE, MouseDriverTdCheckFun); // Hardcoded for now
 
@@ -63,12 +72,14 @@ mouseProfile_t EFIAPI initLogitechGProSuperlightMouseXHCI(MouseDriverTdCheck Mou
 
 	LOG_VERB("[XHC] Found endpoint Ring %p\r\n", endpointRing);
 
-	// Fix: loop is not needed, the ring is filled from top to bottom.
+	// Find transferBuffer
+	// loop is not needed, the ring is filled from top to bottom.
 	// --> if there is no data at the top, there will not be data in the bottom either.
 	EFI_PHYSICAL_ADDRESS TDPointerPhys = 0;
 	EFI_PHYSICAL_ADDRESS TDPointerPhys2 = 0;
 
 	// Copy the first and the second transfer buffer pointers (they are positioned with a distance of 0x20 between)
+	// We use 0x20 steps, as upon a Normal TRB (Intel eXtensible Host Controller Interface for USB, 4.11.2.1, Page 211, Normal TRB), we have always observed an Event Data TRB (Intel eXtensible Host Controller Interface for USB, 4.11.5.2, Page 230, Event Data TRB)
 	pMemCpyForce((EFI_PHYSICAL_ADDRESS)&TDPointerPhys, endpointRing, sizeof(EFI_PHYSICAL_ADDRESS));
 	pMemCpyForce((EFI_PHYSICAL_ADDRESS)&TDPointerPhys2, endpointRing + 0x20, sizeof(EFI_PHYSICAL_ADDRESS));
 
@@ -94,11 +105,12 @@ mouseProfile_t EFIAPI initLogitechGProSuperlightMouseXHCI(MouseDriverTdCheck Mou
 	pMemCpyForce((EFI_PHYSICAL_ADDRESS)&tempIdentifierSL, TDPointerPhys + 0x8, sizeof(UINT32));
 	pMemCpyForce((EFI_PHYSICAL_ADDRESS)&tempIdentifier2SL, TDPointerPhys2 + 0x8, sizeof(UINT32));
 
-	// Check if it equals to 0x409301
+	// Check if they equal to 0x409301
 	if ((tempIdentifierSL == 0x409301) && (tempIdentifier2SL == 0x409301))
 	{
 		// Now set all global variables so we dont have to recalculate them when doing the actual manipulation
 		mouseProfile_t ret_valid = { TRUE, TDPointerPhys, TDPointerPhys, TDPointerPhys + 0x2, TDPointerPhys2, TDPointerPhys2, TDPointerPhys2 + 0x2 };
+
 		// Return the pointer to the transfer buffer to indicate successful execution
 		return ret_valid;
 	}
